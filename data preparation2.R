f4e44bbce49ec163358a4df7e3d8a0cca3e5f8fe
CrisFraile <crisfrfe@gmail.com>
2019-03-20 14:39:16 +0100

    Data preparation second data set

---
+++ data preparation2.R
@@ -0,0 +1,21 @@
+data=read.csv("googleplaystore_user_reviews.csv")
+(head(data))
+class(data)
+(summary(data$App))
+class(tab)
+head(tab)
+
+# elimino la segunda columna
+# quito las filas con NA
+
+data2=data %>%
+  drop_na() %>%
+  #group_by(Sentiment)%>%
+  select(-Translated_Review,-Sentiment_Polarity,-Sentiment_Subjectivity)
+#columnas guya para agrupar por appsssss
+valor = data2 %>% group_by(App)%>%mutate(countpos = sum(Sentiment=='Positive')) %>%
+  mutate(countneut = sum(Sentiment=='Neutral')) %>% 
+  mutate(counteg = sum(Sentiment=='Negative'))%>%select(-Sentiment)
+
+valor2= valor[!duplicated(valor),]
+tablefinal=valor2[-2,]
